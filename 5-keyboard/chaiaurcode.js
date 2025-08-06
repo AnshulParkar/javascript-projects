4f8a7924bc9469de0e4ec2346b784e40d51e8a31
Anshul Parkar <121174809+AnshulParkar@users.noreply.github.com>
2025-08-06 16:25:26 +0530

    Update js file

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,20 @@
-console.log('Project 5');
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+    <div>
+      <table border="1" cellpadding="8" cellspacing="0">
+        <tr>
+          <th>Key</th>
+          <th>Keycode</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === ' ' ? 'Space' : e.key}</td>
+          <td>${e.keyCode}</td>
+          <td>${e.code}</td>
+        </tr>
+      </table>
+    </div>
+  `;
+});
